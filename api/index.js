8e97ea6bc1537275b5b1200199d3f0a796148f1d
your Nmae <you@example.com>
2025-04-04 21:00:53 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from'mongoose';
 import dotenv from 'dotenv';
-import userRoute from './routes/user.route.js'; 
+import userRoutes from './routes/user.route.js'; 
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -19,4 +20,5 @@ app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
 });
 
-app.use('/api/user',userRoute);
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
