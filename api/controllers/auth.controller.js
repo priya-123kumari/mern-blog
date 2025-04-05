fedddd552a91d70997a0e3ac7747329bd541a219
your Nmae <you@example.com>
2025-04-05 16:04:35 +0530

    commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,18 @@
-export const signup=async(req,res)=>{
-    console.log(req.body); 
+import User from "../models/User.model.js";
 
-}
+export const signup=async(req,res)=>{
+    const {username,email,password}=req.body;
+    if(!username || !email || !password || username===" " || email===" " || password===" "){
+        return res.status(400).json({message:" all fields are required"});
+    }
+    const newUser=new User({
+        username, 
+        email, 
+        password,
+      });
+    
+      await newUser.save();
+      res.json("signup successfully");
+   
+
+};
